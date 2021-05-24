a17ac85acd924a3f72c8aad355aa22372fb51970
Chris Paldino <cpaldino9@gmail.com>
2021-05-24 00:52:53 -0400

    fixed data fetching

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const axios = require("axios");
+
+app.use(cors());
+
+app.get("/api/",async(req,res)=>{
+    const url = 'https://gist.githubusercontent.com/cpal39/7081859ea04b90ac2522e84ea6189f3b/raw/4814c4eb63ecfbdd5bcc32a20bf4c9cb72aa0f2e/data.json';
+	try{
+		const {data}=await axios.get(url);
+        res.status(200).json(data);
+	}
+	catch(e){
+		res.status(404);
+	}
+});
+
+app.use("*", (req, res) => {
+	res.status(404).json({ error: "Not found" });
+});
+
+app.listen(5000, () => {
+	console.log("We've now got a server!");
+	console.log("Your routes will be running on http://localhost:5000");
+});
--- src/components/main.js
+++ src/components/main.js
@@ -1,20 +1,38 @@
 import ContentCard from './contentCard';
 import Details from './details'
 import { useState, useEffect } from 'react';
-const dataList = require('../localData/data.json');
+import localData from '../localData/data.json';
+const axios=require("axios");
 
 export default function Main() {
-    const [data, setData] = useState(dataList)
+    const [data, setData] = useState([]);
+    const url="http://localhost:5000/api";
+    useEffect(() => {
+        async function getData(url) {
+            try {
+                const response = await axios.get(url);
+                const remoteData = response.data;
+                console.log(response);
+                console.log(remoteData);
+                setData(remoteData);
+            }
+            catch (e) {
+                console.log(`error ${e}. Using local data instead...`);
+                setData(localData);
+            }
+        }
+        getData(url);
+    });
     return (
         <div>
             <div className="container">
                 <div className="row">
                     {data.map((item, id) => {
                         return <div key={id} className="col-lg-3 col-md-6 col-sm-12 cardDiv">
-                            <ContentCard data={item}/>
-                            <Details id={item.id+"_desc"} title={item.name+" Description"} body={item.description}/>
-                            <Details id={item.id+"_sol"} title={item.name+" Solution"} body={item.solution}/>
-                            <Details id={item.id+"_evid"} title={item.name+" Evidence"} body={item.evidence}/>
+                            <ContentCard data={item} />
+                            <Details id={item.id + "_desc"} title={item.name + " Description"} body={item.description} />
+                            <Details id={item.id + "_sol"} title={item.name + " Solution"} body={item.solution} />
+                            <Details id={item.id + "_evid"} title={item.name + " Evidence"} body={item.evidence} />
                         </div>
                     })}
                 </div>
